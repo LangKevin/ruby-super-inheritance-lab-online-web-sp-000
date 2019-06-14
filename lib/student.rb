390243c552f046ed0e7727beb712578e90b966c2
kevin lang <langkevina@gmail.com>
2019-06-14 00:28:34 +0000

    Automatically backed up by Learn

---
+++ lib/student.rb
@@ -0,0 +1,16 @@
+class Student
+
+  def initialize
+  end
+
+  def hello
+    "Hey there! I'm so excited to learn stuff.\n"
+  end
+
+  def raise_hand
+    # "Hello \"world\"!"
+    # => "Hello \"world\"!"
+    "Pick me!\"\n\""
+  end
+
+end
