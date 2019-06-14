7892d86fdfc1e8370047965a408d200ffea9dc92
kevin lang <langkevina@gmail.com>
2019-06-14 00:35:53 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,8 +4,7 @@ class ChattyStudent < Student
   end
 
   def hello
-    super + "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
-  end
+    puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
 
   def raise_hand
     returnStr = ""
